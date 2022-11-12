fce0f23f65c732bc7d5f3bb6d7e2b935fb6dbe87
continuetostop <phuc.ml.dl@gmail.com>
2022-11-12 15:54:00 +0700

    update port

--- app/config/Database.js
+++ app/config/Database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 let DBConnectorSetting = [
     {
         host: process.env.DBHOST||'remotemysql.com',
-        port:'3306',
+        port:3306,
         username:process.env.DBUSERNAME||'1baCA5TYVo',
         password:process.env.DBPASSWORD||'inDfxhNebE',
         database:process.env.DBDATABASE||'1baCA5TYVo',
